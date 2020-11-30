4148704a704d2bc570a84b67c895b9f648166820
direboar <minokuba@example.com>
2020-11-30 23:55:31 +0900

    apply serverless-layers plugin

--- myService/handler.js
+++ myService/handler.js
@@ -1,6 +1,8 @@
 'use strict';
+const moment = require('moment');
 
 module.exports.hello = async event => {
+  // throw new Error("error")
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -11,6 +13,17 @@ module.exports.hello = async event => {
       null,
       2
     ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.goodby = async event => {
+  // throw new Error("error")
+  return {
+    statusCode: 200,
+    body: `現在時刻は${moment().format()}です`
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
